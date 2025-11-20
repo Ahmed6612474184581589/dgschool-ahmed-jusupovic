924b2cebc165a71eb2a329127ccbead0a193f0d7
Ahmed6612474184581589 <ahmohodza74@gmail.com>
2025-11-20 19:36:10 +0100

    a

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,10 +5,24 @@ const Home = ({ navigation }) => {
     return(
         <View style={styles.container}>
             <Text>Welcome to Home Screen</Text>
-            <Button
-                title="Go to About"
-                onPress={() => navigation.navigate("About")}
-            />
+             <Swiper autoplay={true} activeDotColor="#22D4FF" autoplayTimeout={5}>
+                <View style={styles.item}>
+                    <Image style={styles.imgItem} source={require("../..assets/banner_1.jpg")}
+                    resitemode="cover"></Image>
+                </View>
+                 <View style={styles.item}>
+                    <Image style={styles.imgItem} source={require("../..assets/banner_1.jpg")}
+                    resitemode="cover"></Image>
+                </View>
+                 <View style={styles.item}>
+                    <Image style={styles.imgItem} source={require("../..assets/banner_1.jpg")}
+                    resitemode="cover"></Image>
+                </View>
+                 <View style={styles.item}>
+                    <Image style={styles.imgItem} source={require("../..assets/banner_1.jpg")}
+                    resitemode="cover"></Image>
+                </View>
+            </Swiper>
         </View>
     );
 };
@@ -18,6 +32,10 @@ const styles = StyleSheet.create({
         flex:1,
         backgroundColor:"#fff",
         alignItems:"center",
+        justifyContent:"center",
+    },
+    item:{
+        flex:1,
         justifyContent:"center",
     },
 });
