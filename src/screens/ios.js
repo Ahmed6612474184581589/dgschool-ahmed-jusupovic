abe808e6f29d30120d06d782d3347e87c7b09e96
Ahmed6612474184581589 <ahmohodza74@gmail.com>
2025-12-04 20:05:54 +0100

    ios js

---
+++ src/screens/ios.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import {View, Text, StyleSheet, Flatlist} from "react-native";
+import data from "../data/data.json";
+
+
+
+class ios extends Component{
+    componentDidMount(){
+        products: data,
+    }
+}
